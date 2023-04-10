905b167dbf3b3a76b1712feb973ebbe3f1eb4b05
Brandon Yaeck <yaeckb@sheridancollege.ca>
2023-04-09 20:59:12 -0400

    set up card class, remove abstract

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -11,15 +11,52 @@ package ca.sheridancollege.project;
  * @author dancye
  * @author Brandon Yaeck, April 2023
  */
-public abstract class Card {
+public class Card {
 	//default modifier for child classes
+	public enum Suit {HEARTS, DIAMONDS, CLUBS, SPADES};
 
+	// 3 is the weakest and 2 is the strongest
+	public enum Rank {
+		THREE(1), FOUR(2), FIVE(3), SIX(4), SEVEN(5), EIGHT(6), NINE(7), TEN(8), JACK(9), QUEEN(10), KING(11), ACE(12), TWO(13);
+
+		public final int value;
+
+		private Rank(int value) {
+			this.value = value;
+		}
+	};
+
+	private Suit suit;
+	private Rank rank;
+
+	public Card(Suit suit, Rank rank) {
+		this.suit = suit;
+		this.rank = rank;
+	}
+
+	public Suit getSuit() {
+		return suit;
+	}
+
+	public void setSuit(Suit suit) {
+		this.suit = suit;
+	}
+
+	public Rank getRank() {
+		return rank;
+	}
+
+	public void setRank(Rank rank) {
+		this.rank = rank;
+	}
+	
 	/**
-	 * Students should implement this method for their specific children classes
 	 *
-	 * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
+	 * @return a String representation of a card.
 	 */
 	@Override
-	public abstract String toString();
+	public String toString() {
+		return rank + " of " + suit;
+	}
 
 }

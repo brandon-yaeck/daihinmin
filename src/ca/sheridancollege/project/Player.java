f1c80cb71b25c215f77d06299d09b2c423b82de5
Brandon Yaeck <yaeckb@sheridancollege.ca>
2023-04-17 16:49:06 -0400

    add gameplay logic for players

--- src/ca/sheridancollege/project/Player.java
+++ src/ca/sheridancollege/project/Player.java
@@ -5,6 +5,9 @@
  */
 package ca.sheridancollege.project;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * A class that models each Player in the game. Players have an identifier, which should be unique.
  *
@@ -18,9 +21,9 @@ public class Player implements Comparable<Player> {
 	private PlayerRank rank; //the current rank of the player
 	private Hand hand = new Hand();
 
-	// daihinmin is the lowest, daifugou is the highest
+	// daihinmin is the best, daifugou is the worst
 	public enum PlayerRank {
-		DAIHINMIN(1), HINMIN(2), HEIMIN(3), FUGOU(4), DAIFUGOU(5);
+		DAIFUGOU(1), FUGOU(2), HEIMIN(3), HINMIN(4), DAIHINMIN(5);
 
 		public final int value;
 
@@ -70,9 +73,83 @@ public class Player implements Comparable<Player> {
 
 	/**
 	 * The method to be overridden when you subclass the Player class with your specific type of Player and filled in with logic to play your game.
+	 * 
+	 * @return Whether a card was played
 	 */
-	public void play() {
+	public boolean play(ArrayList<Card> playArea, int trickSize, Card.Rank lastCard) {
+		Scanner keyboard = new Scanner(System.in);
 
+
+
+		ArrayList<Card.Rank> selectedRanks;
+		switch (trickSize) {
+			case 4:
+				selectedRanks = hand.getQuadruples();
+				break;
+			case 3:
+				selectedRanks = hand.getTriples();
+				break;
+			case 2:
+				selectedRanks = hand.getDoubles();
+				break;
+			default:
+				selectedRanks = hand.getSingles();
+				break;
+		}
+
+		System.out.printf("%d cards of the same rank must be played.\n", trickSize);
+		
+
+
+		while (true) {
+			System.out.printf("(%s) Enter card rank to play (or pass): ", name);
+			try {
+				String input = keyboard.nextLine().trim().toUpperCase();
+
+				if (input.equals("PASS")) {
+					return false;
+				} else {
+					Card.Rank selection = Card.Rank.valueOf(input);
+					if (selectedRanks.contains(selection)) {
+						if (playArea.isEmpty() || selection.value > lastCard.value) {
+							// find card in hand that matches
+							int matchAmount = 0;
+							for (Card card: hand.getCards()) {
+								if (card.getRank() == selection) {
+									playArea.add(card);
+									matchAmount++;	
+								}
+
+								// continue looping according to how many cards were played in order to find them all
+								// limit amount of cards played to trick size
+								if (matchAmount == trickSize) {
+									break;
+								}						
+							}
+							// remove cards from hand if they are in the play area
+							// this only works with a single deck in use
+							hand.getCards().removeAll(playArea);
+
+							break;
+						} else {
+							System.out.printf("%s is lower than the current cards in play\n", selection);
+						}
+					} else {
+						System.out.printf("%s is not available to play\n", selection);
+					}
+				}
+			}
+			catch (IllegalArgumentException exception) {
+				System.out.printf("%s\n", exception.getMessage());
+			}
+		}
+		System.out.println();
+
+		// remove the cards from the duplicate list (will be regenerated next loop)
+		hand.clearRankLists();
+
+		// card successfully played if reached end
+		return true;
 	}
 
 	/**

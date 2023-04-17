7730e4919241d5cd7022eb8b1109632d4dc13f03
Brandon Yaeck <yaeckb@sheridancollege.ca>
2023-04-17 16:47:13 -0400

    process trick sizes in Hand

--- src/ca/sheridancollege/project/Hand.java
+++ src/ca/sheridancollege/project/Hand.java
@@ -58,11 +58,59 @@ public class Hand extends GroupOfCards {
 		return quadrupleCards;
 	}
 
-	@Override
-	public void clearCards() {
-		cards.clear();
+	public void clearRankLists() {
+		singleCards.clear();
 		doubleCards.clear();
 		tripleCards.clear();
 		quadrupleCards.clear();
+	}
+
+	@Override
+	public void clearCards() {
+		cards.clear();
+		clearRankLists();
+	}
+
+	public int getTrickSizes() {
+		//determine available trick sizes in the current hand
+		// previousCard stores the last one checked in the hand to check for duplicates
+		Card previousCard = null;
+		int duplicateCount = 0;
+		for (Card card: cards) {
+			if (previousCard != null && card.compareTo(previousCard) == 0) {
+				switch (duplicateCount) {
+					case 0:
+						addDoubles(card.getRank());
+						break;
+					case 1:
+						addTriples(card.getRank());
+						break;
+					case 2:
+						addQuadruples(card.getRank());
+						break;
+					default:
+						break;
+				}
+				duplicateCount++;
+			} else {
+				duplicateCount = 0;
+			}
+			previousCard = card;
+		}
+		// register all cards as singles as well
+		addSingles();
+
+
+		// return the max trick size based on the above
+		if (!getQuadruples().isEmpty()) {
+			return 4;
+		} else if (!getTriples().isEmpty()) {
+			return 3;
+		} else if (!getDoubles().isEmpty()) {
+			return 2;
+		} else {
+			return 1;
+		}
+
 	}
 }

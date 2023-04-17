1dafa0b0962386122b7996b3f0dcf757b0fab694
Brandon Yaeck <yaeckb@sheridancollege.ca>
2023-04-17 16:49:50 -0400

    add tricks/rounds gameplay loop

--- src/ca/sheridancollege/project/Daihinmin.java
+++ src/ca/sheridancollege/project/Daihinmin.java
@@ -1,6 +1,8 @@
 package ca.sheridancollege.project;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 /**
  *
@@ -11,7 +13,7 @@ public class Daihinmin extends Game {
 	private Deck deck = Deck.getInstance();
 	private PlayArea playArea = PlayArea.getInstance();
 
-	private int round = 0;
+	private int round = 1;
 
 	public Daihinmin() {
 		super("Daihinmin");
@@ -39,17 +41,13 @@ public class Daihinmin extends Game {
 		return round;
 	}
 
-	public void setRound(int round) {
-		this.round = round;
-	}
-	
-
-
-	
 	/**
-	 * Deal the deck of cards at the beginning of each round.
+	 * Deal the deck of cards.
+	 * to be called at the beginning of each round.
 	 */
 	public void deal() {
+		deck.shuffle();
+
 		int i = 0;
 		// iterate through every card in deck
 		for (Card card: deck.getCards()) {
@@ -67,12 +65,162 @@ public class Daihinmin extends Game {
 	/**
 	 * Play the game. This might be one method or many method calls depending on your game.
 	 */
+	@Override
 	public void play() {
+		// 5 rounds by default
+		play(5);
+	}
+
+	public void play(int totalRounds) {
+		while (round <= totalRounds) {
+			// force card trading here if required ranks exist
+			forceCardSwap();
+
+			// deal to each player
+			deal();
+
+			// begin the round
+			playRound();
+
+			round++;
+		}
+		declareWinner();
+	}
+
+	// Main gameplay loop
+	public void playRound() {
+		int i = 0;	// index of the current player
+		int trickSize = 1;
+		Player lastPlay = null;
+		Player currentPlayer;
+		Card.Rank lastCard = null;
+		ArrayList<Player> roundRanking = new ArrayList<>();
+		int passCount = 0;
+
+		while (true) {
+			currentPlayer = players.get(i);
+
+			// check if round is over
+			if (roundRanking.size() == players.size() - 1) {
+				// rank the last remaining player
+				roundRanking.add(currentPlayer);
+
+				// process final round rankings
+				rankPlayers(roundRanking);
+
+				// clean up
+				currentPlayer.getHand().clearCards();
+				playArea.clearCards();
+
+				break;
+			}
+
+
+			// check for trick end if all players pass
+			if (passCount == players.size()) {
+				// last person to successfully play goes first in next trick
+				currentPlayer = lastPlay;
+				// also have to set the index for the next player to be correct when looping
+				i = players.indexOf(currentPlayer);
+
+				// VISUAL BUG HERE:
+				// if a player empties their hand and is the last player to play in the trick, the output will say they start the next trick
+				// doesn't affect gameplay as it will auto pass to the next player anyway
+
+				System.out.println();
+				System.out.println("=======================================================");
+				System.out.printf("Trick has ended. %s starts the next trick.\n", currentPlayer.getName());
+				System.out.println("=======================================================");
+
+				//clean up for next trick
+				lastCard = null;
+				trickSize = 1;
+				playArea.clearCards();
+			}
+
+			// if last card was a 2 or hand empty then auto pass
+			if (lastCard != Card.Rank.TWO && !currentPlayer.getHand().getCards().isEmpty()) {
+
+				currentPlayer.getHand().sortHand();
+
+				// show playing field:
+				// NOTE: playing field never needs to be sorted as cards are always added in ascending order
+				GroupOfCardsView.show("Playing Field", playArea.getCards());
+
+				// show current player's hand
+				GroupOfCardsView.show(currentPlayer.getName() + "'s hand", currentPlayer.getHand().getCards());
+
+				//determine available trick sizes in the current hand and get the max size
+				int maxTrickSize = currentPlayer.getHand().getTrickSizes();
+
+				//get the trick size to be used for the trick if field is empty and player has a trick size greater than 1
+				if (playArea.getCards().isEmpty() && maxTrickSize > 1) {
+					trickSize = getTrickSize(maxTrickSize, currentPlayer.getName());
+				} 
+
+				// player plays
+				if (currentPlayer.play(playArea.getCards(), trickSize, lastCard)) {
+					// keep track of the last player to play a card successfully
+					lastPlay = currentPlayer;
+					// reset pass count since a card was successfully played
+					passCount = 0;
+				} else {
+					passCount++;
+				}
+
+				// check if player emptied hand
+				if (currentPlayer.getHand().getCards().isEmpty()) {
+					System.out.println();
+					System.out.println("=======================================================");
+					System.out.println(currentPlayer.getName() + " has emptied their hand.");
+					System.out.println("=======================================================");
+
+					// rank the player
+					roundRanking.add(currentPlayer);
+				}
+			} else {
+				passCount++;
+			}
+
+			// keep track of what the rank of the last card placed in the playing area is
+			if (!playArea.getCards().isEmpty()) {
+				lastCard = playArea.getCards().get(playArea.getCards().size() - 1).getRank();
+			}
+
+			
+			// return to first player after each have played
+			if (i < players.size() - 1) {
+				i++;
+			} else {
+				i = 0;
+			}
+		}
 
 	}
 
-	public void rankPlayers() {
+	public void rankPlayers(ArrayList<Player>roundRanking) {
+		// announce results
+		System.out.println();
+		System.out.println();
+		System.out.println("The round has ended.");
+		System.out.println();
+		System.out.println("New player rankings:");
+		System.out.println();
 
+		// can't loop, bottom 2 players must always be last 2 ranks as player count might not line up with rank count
+		roundRanking.get(0).setRank(Player.PlayerRank.DAIFUGOU);
+		roundRanking.get(1).setRank(Player.PlayerRank.FUGOU);
+		roundRanking.get(roundRanking.size() - 2).setRank(Player.PlayerRank.HINMIN);
+		roundRanking.get(roundRanking.size() - 1).setRank(Player.PlayerRank.DAIHINMIN);
+
+		// for more than 4 players, the rest get HEIMIN, but not implementing now
+
+		// order by player rank
+		Collections.sort(players);
+
+		for (Player player: players) {
+			System.out.printf("%s: %s\n", player.getName(), player.getRank());
+		}
 	}
 
 	public void forceCardSwap() {
@@ -82,7 +230,42 @@ public class Daihinmin extends Game {
 	/**
 	 * When the game is over, use this method to declare and display a winning player.
 	 */
+	@Override
 	public void declareWinner() {
+		System.out.println();
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		// daifugou player is always placed as the first index in the list
+		System.out.printf("%s wins the game for being the last daifugou!", players.get(0));
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		System.out.println();
+	}
 
+	public int getTrickSize(int maxTrickSize, String playerName) {
+		Scanner keyboard = new Scanner(System.in);
+		int trickSize = 1;
+
+		while (true) {
+			System.out.printf("(%s) Enter amount of cards to play [1 - %d]: ", playerName, maxTrickSize);
+			try {
+				// choose trick size
+				trickSize = Integer.parseInt(keyboard.nextLine());
+
+				if (trickSize < 1 || trickSize > maxTrickSize) {
+					throw new IllegalArgumentException("Amount of cards to play must be between 1 and " + maxTrickSize + ".");
+				}
+				else {
+					System.out.println();
+					break;
+				}
+			}
+			catch (NumberFormatException exception) {
+				System.out.printf("You must enter a number.\n");
+			}
+			catch (IllegalArgumentException exception) {
+				System.out.printf("%s\n", exception.getMessage());
+			}
+		}
+
+		return trickSize;
 	}
 }

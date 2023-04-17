95266eec20ad058aaef4a13c792bff2da9a16014
Brandon Yaeck <yaeckb@sheridancollege.ca>
2023-04-16 23:06:32 -0400

    Detect duplicate cards available in hand

--- src/ca/sheridancollege/project/Hand.java
+++ src/ca/sheridancollege/project/Hand.java
@@ -1,5 +1,6 @@
 package ca.sheridancollege.project;
 
+import java.util.ArrayList;
 import java.util.Collections;
 
 /**
@@ -8,6 +9,17 @@ import java.util.Collections;
  */
 public class Hand extends GroupOfCards {
 
+	private ArrayList<Card.Rank> singleCards;
+	private ArrayList<Card.Rank> doubleCards;
+	private ArrayList<Card.Rank> tripleCards;
+	private ArrayList<Card.Rank> quadrupleCards;
+
+	public Hand() {
+		singleCards = new ArrayList<>();
+		doubleCards = new ArrayList<>();
+		tripleCards = new ArrayList<>();
+		quadrupleCards = new ArrayList<>();
+	}
 	/**
 	 * Sorts the hand for visual clarity.
 	 * 
@@ -15,5 +27,42 @@ public class Hand extends GroupOfCards {
 	public void sortHand() {
 		Collections.sort(cards);
 	}
-	
+
+	public void addSingles() {
+		for (Card card: cards) {
+			singleCards.add(card.getRank());
+		}
+	}
+	public ArrayList<Card.Rank> getSingles() {
+		return singleCards;
+	}
+
+	public void addDoubles(Card.Rank rank) {
+		doubleCards.add(rank);
+	}
+	public ArrayList<Card.Rank> getDoubles() {
+		return doubleCards;
+	}
+
+	public void addTriples(Card.Rank rank) {
+		tripleCards.add(rank);
+	}
+	public ArrayList<Card.Rank> getTriples() {
+		return tripleCards;
+	}
+
+	public void addQuadruples(Card.Rank rank) {
+		quadrupleCards.add(rank);
+	}
+	public ArrayList<Card.Rank> getQuadruples() {
+		return quadrupleCards;
+	}
+
+	@Override
+	public void clearCards() {
+		cards.clear();
+		doubleCards.clear();
+		tripleCards.clear();
+		quadrupleCards.clear();
+	}
 }

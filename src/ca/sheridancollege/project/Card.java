a640d5872612d67b87e0f4133d86659b2dc2b16c
Brandon Yaeck <yaeckb@sheridancollege.ca>
2023-04-19 22:05:12 -0400

    added javadoc comments

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -12,10 +12,16 @@ package ca.sheridancollege.project;
  * @author Brandon Yaeck, April 2023
  */
 public class Card implements Comparable<Card> {
-	//default modifier for child classes
+	/**
+	 * List of available suits for each card.
+	 */
 	public enum Suit {HEARTS, DIAMONDS, CLUBS, SPADES};
 
-	// 3 is the weakest and 2 is the strongest
+	/**
+	 * List of available ranks for each card.
+	 * given an integer value for comparison purposes.
+	 * 3 is the weakest and 2 is the strongest.
+	 */
 	public enum Rank {
 		THREE(1), FOUR(2), FIVE(3), SIX(4), SEVEN(5), EIGHT(6), NINE(7), TEN(8), JACK(9), QUEEN(10), KING(11), ACE(12), TWO(13);
 
